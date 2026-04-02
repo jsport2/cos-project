dcf7b9e8e3ed600c13f2faae7b5b00423a782891
Jeongguk Park <jsport@kentech.ac.kr>
2026-04-02 00:05:58 +0000

    20260330_update

---
+++ 5/practice.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main(int argc, char *argv[])
+{
+	char str1[6] = {'h','e','l','l','o'};
+	char str2[6] = {'w','o','r','l','d'};
+	char *p;
+	int i;
+
+	p = str2;
+
+	cout << "str1 = " << str1 << endl;
+	cout << "str2 = " << str2 << endl;
+	p[-1] = ',';
+	cout << "str1 = " << str1 << endl;
+	return 0;
+}
